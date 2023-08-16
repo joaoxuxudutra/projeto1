823932e76af5b4d86a147402bb7c26fe53b55ee9
joaoxuxudutra <133870112+joaoxuxudutra@users.noreply.github.com>
2023-08-16 13:35:29 +0000

    aula

--- App.js
+++ App.js
@@ -1,108 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
-
-
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker'
 export default function App() {
+  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
+  const [moedaDestino, setMoedaDestino] = useState('USD')
+  const [valorConvertido, setValorConvertido] = useState('')
 
-  const[moedaOrigem, setMoedaOrigem] = useState('')
-  const[moedaDestino, setMoedaDestino] = useState('')
-
-  return (
-      <View style={styles.container}>
-        <View style={styles.content}>
-        <Text>Moeda 1</Text>
-        <TextInput placeholder='moeda 1' style={styles.input}/>
-
-        <Picker
-          selectedValue= {moedaOrigem}
-          onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
-          >
-            <Picker.Item label="" value=""/>
-            <Picker.Item label="" value=""/>
-            <Picker.Item label="" value=""/>
-            <Picker.Item label="" value=""/>
-
-        </Picker>
-
+  const buscarHandle = async () => {
+    let URL = `https://economia.awesomeapi.com.br/last/USD-BRL`
+    try {
+      let page = await fetch(URL)
+      let json = await page.json()
+      //console.log(json)
+      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high)
+      console.log(indice)
+    } catch (error) {
         
+      
+    }
+    // setValorConvertido(URL);
+  };
 
-        <Text>Moeda 2</Text>
-        <TextInput placeholder='moeda 2' style={styles.input}/>
-
+  const limparResultado = ()=> {
+    setValorConvertido('')
+  };
+  
+  return (
+    <View style={styles.container}>
+      <Text>Conversor de Moedas</Text>
+      <View>
+        <Text>Moeda 1</Text>
         <Picker
-           selectedValue= {moedaDestino}
-           onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
+          style={{ height: 50, width: 200 }}
+          selectedValue={moedaOrigem}
+          onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
         >
-          <Picker.Item label="" value=""/>
-          <Picker.Item label="" value=""/>
-          <Picker.Item label="" value=""/>
-          <Picker.Item label="" value=""/>
-
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dólar Americano" value="USD" />
+          <Picker.Item label="Ouro" value="XAU" />
+          <Picker.Item label="Bitcoin" value="BTC" />
         </Picker>
-
-          <Pressable style={styles.botao}>
-              <Text style={styles.botaoTexto}>Calcular</Text>
-          </Pressable>
-              <Text>Resultado</Text>
-
-        </View>
       </View>
+      <View>
+        <Text>Moeda 2</Text>
+        <Picker
+          style={{ height: 50, width: 200 }}
+          selectedValue={moedaDestino}
+          onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
+        >
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dólar Americano" value="USD" />
+          <Picker.Item label="Ouro" value="XAU" />
+          <Picker.Item label="Bitcoin" value="BTC" />
+        </Picker>
+      </View>
+      <Pressable onPress={buscarHandle}>
+        <Text>Buscar Valor</Text>
+        </Pressable>
+      <Text>{`Resultado: ${valorConvertido}`}</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    padding: 16,
-    color: '#000'
-  },
-
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#84E1DF',
-    
-  },
-
-
-  input:{
-    color:'#000',
-    fontSize: 19,
-    height: 50,
-    padding: 10,
-    width: 180,
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-
-  botaoTexto: {
-    flexDirection: 'row',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 100,
-    elavation: 3,
-    backgroundColor: 'white',
-    marginTop: 10,
-    height: 60,
   },
-  textoBotao: {
-    color: 'black'
-  },
-  botaoResultado:{
-     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 100,
-    elavation: 3,
-    backgroundColor: 'white',
-    marginTop: 10,
-    height: 60,
-  },
-
-  },
-);
+});
